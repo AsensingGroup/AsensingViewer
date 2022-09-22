8d5441e8b32dac82f7cb097888e4e50e3b515d54
luhuadong <luhuadong@ag.com>
2022-09-22 16:03:22 +0800

    [Interpreter] formatting Asensing_parser.lua

--- Plugins/AsensingPlugin/Utilities/Asensing_parser.lua
+++ Plugins/AsensingPlugin/Utilities/Asensing_parser.lua
@@ -28,157 +28,157 @@ function lidar_proto.dissector(buffer,pinfo,tree)
 	local tailSize = 4
 
 	-- Packet Header --
-	local header_subtree = subtree:add(buffer(curr,headerSize),"Header")
+	local header_subtree = subtree:add(buffer(curr, headerSize), "Header")
 
 	local Sob = buffer(curr, 4):uint()
-	header_subtree:add(buffer(curr, 4),"Sob : " .. Sob)
+	header_subtree:add(buffer(curr, 4), "Sob : " .. Sob)
 	curr = curr + 4
 
 	local FrameID = buffer(curr, 4):uint()
-	header_subtree:add(buffer(curr, 4),"FrameID : " .. FrameID)
+	header_subtree:add(buffer(curr, 4), "FrameID : " .. FrameID)
 	curr = curr + 4
 
 	local SeqNum = buffer(curr, 2):uint()
-	header_subtree:add(buffer(curr, 2),"SeqNum : " .. SeqNum)
+	header_subtree:add(buffer(curr, 2), "SeqNum : " .. SeqNum)
 	curr = curr + 2
 
 	local PkgLen = buffer(curr, 2):uint()
-	header_subtree:add(buffer(curr, 2),"PkgLen : " .. PkgLen)
+	header_subtree:add(buffer(curr, 2), "PkgLen : " .. PkgLen)
 	curr = curr + 2
 
 	local LidarType = buffer(curr, 2):uint()
-	header_subtree:add(buffer(curr, 2),"LidarType : " .. LidarType)
+	header_subtree:add(buffer(curr, 2), "LidarType : " .. LidarType)
 	curr = curr + 2
 
 	local VersionMajor = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"VersionMajor : " .. VersionMajor)
+	header_subtree:add(buffer(curr, 1), "VersionMajor : " .. VersionMajor)
 	curr = curr + 1
 
 	local VersionMinor = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"VersionMinor : " .. VersionMinor)
+	header_subtree:add(buffer(curr, 1), "VersionMinor : " .. VersionMinor)
 	curr = curr + 1
 
 	local UTCTime0 = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"UTCTime0 : " .. UTCTime0)
+	header_subtree:add(buffer(curr, 1), "UTCTime0 : " .. UTCTime0)
 	curr = curr + 1
 
 	local UTCTime1 = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"UTCTime1 : " .. UTCTime1)
+	header_subtree:add(buffer(curr, 1), "UTCTime1 : " .. UTCTime1)
 	curr = curr + 1
 
 	local UTCTime2 = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"UTCTime2 : " .. UTCTime2)
+	header_subtree:add(buffer(curr, 1), "UTCTime2 : " .. UTCTime2)
 	curr = curr + 1
 
 	local UTCTime3 = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"UTCTime3 : " .. UTCTime3)
+	header_subtree:add(buffer(curr, 1), "UTCTime3 : " .. UTCTime3)
 	curr = curr + 1
 
 	local UTCTime4 = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"UTCTime4 : " .. UTCTime4)
+	header_subtree:add(buffer(curr, 1), "UTCTime4 : " .. UTCTime4)
 	curr = curr + 1
 
 	local UTCTime5 = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"UTCTime5 : " .. UTCTime5)
+	header_subtree:add(buffer(curr, 1), "UTCTime5 : " .. UTCTime5)
 	curr = curr + 1
 
-	local Timestamp = buffer(curr,4):uint()
-	header_subtree:add(buffer(curr,4),"Timestamp  : " .. Timestamp)
+	local Timestamp = buffer(curr, 4):uint()
+	header_subtree:add(buffer(curr, 4), "Timestamp  : " .. Timestamp)
 	curr = curr + 4
 
 	local MeasureMode = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"MeasureMode : " .. MeasureMode)
+	header_subtree:add(buffer(curr, 1), "MeasureMode : " .. MeasureMode)
 	curr = curr + 1
 
 	local LaserNum = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"LaserNum : " .. LaserNum)
+	header_subtree:add(buffer(curr, 1), "LaserNum : " .. LaserNum)
 	curr = curr + 1
 
 	local BlockNum = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"BlockNum : " .. BlockNum)
+	header_subtree:add(buffer(curr, 1), "BlockNum : " .. BlockNum)
 	curr = curr + 1
 
 	local EchoCount = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"EchoCount : " .. EchoCount)
+	header_subtree:add(buffer(curr, 1), "EchoCount : " .. EchoCount)
 	curr = curr + 1
 
 	local TimeSyncMode = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"TimeSyncMode : " .. TimeSyncMode)
+	header_subtree:add(buffer(curr, 1), "TimeSyncMode : " .. TimeSyncMode)
 	curr = curr + 1
 
 	local TimeSyncStat = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"TimeSyncStat : " .. TimeSyncStat)
+	header_subtree:add(buffer(curr, 1), "TimeSyncStat : " .. TimeSyncStat)
 	curr = curr + 1
 
 	local MemsTemp = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"MemsTemp : " .. MemsTemp)
+	header_subtree:add(buffer(curr, 1), "MemsTemp : " .. MemsTemp)
 	curr = curr + 1
 
 	local SlotNum = buffer(curr, 1):uint()
-	header_subtree:add(buffer(curr,1),"SlotNum : " .. SlotNum)
+	header_subtree:add(buffer(curr, 1), "SlotNum : " .. SlotNum)
 	curr = curr + 1
 
 	local PointNum = buffer(curr, 2):uint()
-	header_subtree:add(buffer(curr,2),"PointNum : " .. PointNum)
+	header_subtree:add(buffer(curr, 2), "PointNum : " .. PointNum)
 	curr = curr + 2
 
 	local Reserved1 = buffer(curr, 2):uint()
-	header_subtree:add(buffer(curr,2),"Reserved1 : " .. Reserved1)
+	header_subtree:add(buffer(curr, 2), "Reserved1 : " .. Reserved1)
 	curr = curr + 2
 
 	local Reserved2 = buffer(curr, 2):uint()
-	header_subtree:add(buffer(curr,2),"Reserved2 : " .. Reserved2)
+	header_subtree:add(buffer(curr, 2), "Reserved2 : " .. Reserved2)
 	curr = curr + 2
 
 
 	---- bock Return ----
 	local size = blockPerPacket * blockSize
-	local blockreturns = subtree:add(buffer(curr,size),"Blocks")
+	local blockreturns = subtree:add(buffer(curr, size), "Blocks")
 
 	for i=0, blockPerPacket-1
 	do
-		local block_subtree = blockreturns:add(buffer(curr,blockSize),"Block Return : " ..i)
+		local block_subtree = blockreturns:add(buffer(curr,blockSize), "Block Return : " ..i)
 
 		local channelNum = buffer(curr, 1):uint()
-		block_subtree:add(buffer(curr,1),"channelNum : " .. channelNum)
+		block_subtree:add(buffer(curr, 1), "channelNum : " .. channelNum)
 		curr = curr + 1
 
 		local timeOffSet = buffer(curr, 1):uint()
-		block_subtree:add(buffer(curr,1),"timeOffSet : " .. timeOffSet)
+		block_subtree:add(buffer(curr, 1), "timeOffSet : " .. timeOffSet)
 		curr = curr + 1
 
 		local returnSn = buffer(curr, 1):uint()
-		block_subtree:add(buffer(curr,1),"returnSn : " .. returnSn)
+		block_subtree:add(buffer(curr, 1), "returnSn : " .. returnSn)
 		curr = curr + 1
 
 		local reserved = buffer(curr, 1):uint()
-		block_subtree:add(buffer(curr,1),"reserved : " .. reserved)
+		block_subtree:add(buffer(curr, 1), "reserved : " .. reserved)
 		curr = curr + 1
 		
-		local all_laser_subtree = block_subtree:add(buffer(curr,laserSize * nbLaser),"All Lasers Return")
+		local all_laser_subtree = block_subtree:add(buffer(curr, laserSize * nbLaser), "All Lasers Return")
 
 		for j=0, nbLaser-1
 		do
-			local laser_subtree = all_laser_subtree:add(buffer(curr,laserSize),"Laser Return : " ..j)
+			local laser_subtree = all_laser_subtree:add(buffer(curr, laserSize), "Laser Return : " ..j)
 
-			local Distance = buffer(curr,2):uint()
-			laser_subtree:add(buffer(curr,2),"Distance  : " .. Distance)
+			local Distance = buffer(curr, 2):uint()
+			laser_subtree:add(buffer(curr, 2), "Distance  : " .. Distance)
 			curr = curr + 2
 
-			local Azimuth = buffer(curr,2):uint()
-			laser_subtree:add(buffer(curr,2),"Azimuth  : " .. Azimuth)
+			local Azimuth = buffer(curr, 2):uint()
+			laser_subtree:add(buffer(curr, 2), "Azimuth  : " .. Azimuth)
 			curr = curr + 2
 
-			local Elevation = buffer(curr,2):uint()
-			laser_subtree:add(buffer(curr,2),"Elevation  : " .. Elevation)
+			local Elevation = buffer(curr, 2):uint()
+			laser_subtree:add(buffer(curr, 2), "Elevation  : " .. Elevation)
 			curr = curr + 2
 
-			local Intensity = buffer(curr,1):uint()
-			laser_subtree:add(buffer(curr,1),"Intensity  : " .. Intensity)
+			local Intensity = buffer(curr, 1):uint()
+			laser_subtree:add(buffer(curr, 1), "Intensity  : " .. Intensity)
 			curr = curr + 1
 
-			local Reserved = buffer(curr,2):uint()
-			laser_subtree:add(buffer(curr,2),"Reserved  : " .. Reserved)
+			local Reserved = buffer(curr, 2):uint()
+			laser_subtree:add(buffer(curr, 2), "Reserved  : " .. Reserved)
 			curr = curr + 2
 		end
 
@@ -186,7 +186,7 @@ function lidar_proto.dissector(buffer,pinfo,tree)
 
 
 	-- Tail --
-	local tail_subtree = subtree:add(buffer(curr,tailSize),"Tail")
+	local tail_subtree = subtree:add(buffer(curr, tailSize), "Tail")
 	
 	--[[
 	local CRC = buffer(curr, 4):uint()
@@ -204,7 +204,7 @@ function lidar_proto.dissector(buffer,pinfo,tree)
 	--]]
 
 	local TailFlag = buffer(curr, 4):uint()
-	tail_subtree:add(buffer(curr, 4),"TailFlag : " .. TailFlag)
+	tail_subtree:add(buffer(curr, 4), "TailFlag : " .. TailFlag)
 	curr = curr + 4
 
 end
