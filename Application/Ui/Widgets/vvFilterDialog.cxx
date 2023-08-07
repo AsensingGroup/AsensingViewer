97d46f32f5b0ffbe6408de518cea3b25c9c43cdd
hywing <2112093745@qq.com>
2023-08-07 15:22:02 +0800

    [feat] 增加group分类

--- Application/Ui/Widgets/vvFilterDialog.cxx
+++ Application/Ui/Widgets/vvFilterDialog.cxx
@@ -22,6 +22,9 @@
 #include <QCheckBox>
 #include <QLineEdit>
 #include <QLabel>
+#include <QGroupBox>
+#include <QVBoxLayout>
+#include <QJsonObject>
 
 //-----------------------------------------------------------------------------
 class vvFilterDialog::pqInternal : public Ui::vvFilterDialog
@@ -32,32 +35,40 @@ public:
     {
         this->External = external;
         this->setupUi(external);
+        this->createA2();
+    }
 
+    void createA2() {
+        auto layout = new QVBoxLayout(this->External);
+        auto groupA2 = new QGroupBox("A2", this->External);
+        layout->addWidget(groupA2);
+
+        auto widget = groupA2;
         int beginX = 50;
         int beginY = 50;
         int width = 40;
         int height = 20;
 
         for(int i = 0; i < 4; i++) {
-            auto item = new QCheckBox(external);
+            auto item = new QCheckBox(widget);
             item->setText(QString("face%1").arg(i));
             item->setGeometry(beginX + i * 70, 20, 70, 20);
             this->face[i] = item;
         }
         for(int i = 0; i < 8; i++) {
             for(int j = 0; j < 12; j++) {
-                auto item = new QCheckBox(external);
+                auto item = new QCheckBox(widget);
                 item->setGeometry((width + 10) * i + beginX, (height + 5) * j + beginY, width, height);
                 item->setText(QString::number(i * 12 + j));
                 this->channel[i * 12 + j] = item;
             }
         }
 
-        filterId = new QLineEdit(external);
-        filterId->setGeometry(150, 12 * 25 + 40, 60, 20);
-        auto label = new QLabel(external);
+        filterId = new QLineEdit(widget);
+        filterId->setGeometry(150, 12 * 25 + 50, 60, 20);
+        auto label = new QLabel(widget);
         label->setText("Filter Id :");
-        label->setGeometry(50, 12 * 25 + 40, 100, 20);
+        label->setGeometry(50, 12 * 25 + 50, 100, 20);
     }
 
     void saveSettings();
